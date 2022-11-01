25fa57a829c53a56fc73faf092ccb7fd770ea041
Manfred <manfred.a.mueller@icloud.com>
2022-11-01 12:09:10 +0100

    Fixed formatting issues

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -6,7 +6,6 @@ import 'dart:ui';
 import 'package:flutter/rendering.dart';
 import 'package:pencil_field/pencil_field.dart';
 
-
 /// The pencil can work in two different modes, either draw something or erase
 enum PencilMode { write, erase }
 
